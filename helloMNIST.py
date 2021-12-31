dd2d5b521a768da4147652c7038ac99129a0bb44
MrCocoNuat <aaronwangiscool2@gmail.com>
2021-12-31 14:11:28 -0500

    save and reload

--- helloMNIST.py
+++ helloMNIST.py
@@ -2,48 +2,72 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
-
-mnist = tf.keras.datasets.mnist
+import os
 
 print("using TensorFlow ",tf.__version__)
-
+    
 diagnostic = False
 
-(train_set, train_labels), (test_set, test_labels) = mnist.load_data()
+#like ..
+def parent(path):
+    #check that path is not /, if so return /
+    if len(path) > 1: 
+        path = path[:path[:-1].rfind("/")+1]
+    return path
 
-#normalise to [0,1]
-train_set, test_set = train_set/255, test_set/255
+#path to the script itself
+file_path = os.path.realpath(__file__)
+dir_path = parent(file_path)
 
-if diagnostic:
-    print(train_set.shape)
+#check if model has already been trained
+if os.path.isdir(dir_path+"model"):
+    model = tf.keras.models.load_model(dir_path+"model")
+    print("successfully loaded model from " + dir_path)
 
-
-if diagnostic:
-    plt.figure()
-    plt.imshow(train_set[0])
-    plt.xlabel(train_labels[0])
-    plt.show()
+#redo training
+else:
+    mnist = tf.keras.datasets.mnist
+    
+    (train_set, train_labels), (test_set, test_labels) = mnist.load_data()
+    
+    #normalise to [0,1]
+    train_set, test_set = train_set/255, test_set/255
+    
+    if diagnostic:
+        print(train_set.shape)
+        
+        
+    if diagnostic:
+        plt.figure()
+        plt.imshow(train_set[0])
+        plt.xlabel(train_labels[0])
+        plt.show()
 
     
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28,28)),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(10)
-])
+    model = tf.keras.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28,28)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(10)
+    ])
 
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy']
-              )
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy']
+                  )
 
-model.fit(train_set, train_labels, epochs=5)
+    model.fit(train_set, train_labels, epochs=5)
+    
+    test_loss, test_acc = model.evaluate(test_set, test_labels, verbose=2)
+    
+    print("Accuracy:",test_acc)
 
-test_loss, test_acc = model.evaluate(test_set, test_labels, verbose=2)
+    #done training
+    
+    model.save("model")
+    print("successfully saved model to " + dir_path)
 
-print("Accuracy:",test_acc)
-
-
+    
 prob_model = tf.keras.Sequential([
     model,
     tf.keras.layers.Softmax()
@@ -53,13 +77,13 @@ if diagnostic:
     print(test_set[0])
 
 
-#done training
+
 
     
 #shell mode
 while True:
 
-    filename = input("\nInput .bmp file name OR test_set index? (or exit)")
+    filename = input("\nInput .bmp file name OR test_set index (numeric)? (or exit)")
     if filename == "exit":
         break
 
@@ -73,7 +97,7 @@ while True:
     if mode == "bmp":
     
         if len(filename) <= 4 or filename[-4:] != ".bmp":
-            filename = filename + ".bmp"
+            filename = dir_path + filename + ".bmp"
 
         print("Loading",filename)
         try:
@@ -96,8 +120,21 @@ while True:
         )
 
     elif mode == "test_set":
-        img_array = test_set[index]
-        
+        try:
+            img_array = test_set[index]
+        except NameError:
+            #test_set was not defined because model loaded from dir
+            #recall mnist
+            mnist = tf.keras.datasets.mnist
+    
+            (train_set, train_labels), (test_set, test_labels) = mnist.load_data()
+            
+            #normalise to [0,1]
+            train_set, test_set = train_set/255, test_set/255
+
+            img_array = test_set[index]
+    
+            
     plt.figure()
     plt.imshow(img_array)
     plt.show()
